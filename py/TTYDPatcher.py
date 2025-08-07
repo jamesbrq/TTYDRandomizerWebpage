96d57f974c292716cdd99288ef57e69a1262e9d1
jamesbrq <jamesbrq@gmail.com>
2025-08-07 10:04:11 -0400

    Update TTYDPatcher.py

--- py/TTYDPatcher.py
+++ py/TTYDPatcher.py
@@ -9,18 +9,6 @@ class TTYDPatcher:
     rels: Dict[Rels, io.BytesIO] = {}
 
     def __init__(self):
-        from gclib.gcm import GCM
-        from gclib.dol import DOL
-
-        self.iso = GCM(window.testVar)
-        self.iso.read_entire_disc()
-        self.dol = DOL()
-        self.dol.read(self.iso.read_file_data("sys/main.dol"))
-        for rel in Rels:
-            if rel == Rels.dol:
-                continue
-            path = get_rel_path(rel)
-            self.rels[rel] = self.iso.read_file_data(path)
         print(window.testVar)
 
 def get_rel_path(rel: Rels):
